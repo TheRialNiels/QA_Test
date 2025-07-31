b7c659367f46cd361b6aa1cbf8a3aeb991963f70
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-07-31 13:03:56 -0600

    Add quick test template

---
+++ test/quick.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+
+describe('Array @quickTest@', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      expect([1, 2, 3].indexOf(4)).to.be.equals(-1)
+    })
+
+    it('should return 1 when the value is present', function () {
+      expect([1, 2, 3].indexOf(5)).to.be.equals(1)
+    })
+  })
+})
