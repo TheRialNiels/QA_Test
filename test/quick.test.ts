2b1d8a8b05ebef9187e441c8a6d9253165669bfa
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-08-10 19:18:07 -0600

    Enhance quickTest script with success and failure examples in package-scripts.yml and update corresponding tests

--- test/quick.test.ts
+++ test/quick.test.ts
@@ -2,11 +2,11 @@ import { expect } from 'chai'
 
 describe('Array @quickTest@', function () {
   describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
+    it('should return -1 when the value is not present @successExample@', function () {
       expect([1, 2, 3].indexOf(4)).to.be.equals(-1)
     })
 
-    it('should return 1 when the value is present', function () {
+    it('should return 1 when the value is present @failureExample@', function () {
       expect([1, 2, 3].indexOf(5)).to.be.equals(1)
     })
   })
